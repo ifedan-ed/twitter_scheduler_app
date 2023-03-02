b70b9421de7ad4c9a8fc2aa7aa42fd9b7f626959
ifedan <danielonyejesi@gmail.com>
2023-03-02 09:50:48 +0100

    Password Reset Update Working and able to update Password on the database with token authentication

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,12 +5,33 @@ class PasswordResetsController < ApplicationController
 
   def create
   @user = User.find_by(email: params[:email])
-  if @user.present?
-    #send email notification
-    PasswordMailer.with(user: @user).reset.deliver_now
-  end
+    if @user.present?
+      #send email notification
+      PasswordMailer.with(user: @user).reset.deliver_now
+    end
   redirect_to root_path flash[:notice] = "If You are not trying to trick me My Very Best, an email would be sent to you to change your password"
 
   end
 
+  def edit
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to root_path flash[:alert] = "Your token has expired My Dear, try again will you?"
+  end
+
+  def update
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path flash[:notice] = "My Very Best Your Passord Was Changed Successfully"
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
