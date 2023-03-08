9d61d425118bcb53b82bd762e80ef8136e88ded5
ifedan <danielonyejesi@gmail.com>
2023-03-09 00:33:02 +0100

    Set up sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -69,5 +69,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-
+  config.active_job.queue_adapter = :sidekiq
 end
