5288eda7f7d8845c512e3567d434cc9854a6b66a
ifedan <danielonyejesi@gmail.com>
2023-03-08 22:49:55 +0100

    Added ability to edit tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -7,6 +8,9 @@ class TweetsController < ApplicationController
 
   def new
     @tweet = Tweet.new
+  end
+
+  def edit
   end
 
   def create
@@ -23,5 +27,9 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
