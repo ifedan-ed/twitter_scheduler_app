a3d03bc59ff3a483b6887f84619611555ccc9ff1
ifedan <danielonyejesi@gmail.com>
2023-03-09 15:02:24 +0100

    Send password reset emails with Postmark

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -6,7 +6,7 @@ class PasswordMailer < ApplicationMailer
   #   en.password_mailer.reset.subject
   #
   def reset
-   @token = params[:user].signed_id( purpose: "password_reset", expires_in: 15.minutes)
+    @token = params[:user].signed_id(purpose: "password_reset", expires_in: 15.minutes)
 
     mail to: params[:user].email
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,10 +68,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  #config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
-  config.action_mailer.delivery_method = :postmark
-
-  config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }
   config.active_job.queue_adapter = :sidekiq
 end
