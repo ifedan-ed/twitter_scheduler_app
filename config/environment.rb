bf165b0905897e7698f53f52210e4b22ee15f99b
ifedan <danielonyejesi@gmail.com>
2023-03-09 17:12:31 +0100

    Logs

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.logger = Logger.new(STDOUT)
+config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
